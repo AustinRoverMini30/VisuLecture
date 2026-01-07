8734d664074afa916200366513ade872079c4f79
Nicolas Champy <114686049+AustinRoverMini30@users.noreply.github.com>
2026-01-07 15:52:36 +0100

    Python support on TeacherPanel.razor

---
+++ VisuLecture/StartApp.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+cd /d "%~dp0"
+echo Démarrage de VisuLecture...
+echo.
+echo L'application sera accessible à l'adresse : http://localhost:5188
+echo.
+dotnet run
+
---
+++ VisuLecture/StartAppFromBin.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+cd /d "%~dp0bin\Debug\net9.0"
+echo Démarrage de VisuLecture...
+echo.
+echo L'application sera accessible à l'adresse : http://localhost:5188
+echo.
+echo ATTENTION : Si vous fermez cette fenêtre, l'application s'arrêtera.
+echo.
+start "" "http://localhost:5188"
+VisuLecture.exe
+pause
+
